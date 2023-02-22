0ae2a406face16c43b04b2746afa6f1bc46c3fa5
Saran Warin <119570065+KenFordev@users.noreply.github.com>
2023-02-22 15:51:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const btnEl = document.getElementById("btn");
+const bmiResultEl = document.getElementById("bmi-result");
+const weightConditionEl = document.getElementById("weight-condition");
+
+
+function calculateBMI () {
+    const heightValue = document.getElementById("height").value / 100;
+    const weightValue = document.getElementById("weight").value;
+
+    const bmiValue = weightValue / (heightValue * heightValue);
+
+    bmiResultEl.value = bmiValue;
+
+    if(bmiValue < 18.5){
+        weightConditionEl.innerText = "Under weight";
+    }else if(bmiValue >= 18.5 && bmiValue <= 24.9) {
+        weightConditionEl.innerText = "Normal weight";
+    }else if(bmiValue >= 25 && bmiValue <= 29.9){
+        weightConditionEl.innerText = "Over weight";
+    }else if(bmiValue >= 30) {
+        weightConditionEl.innerText = "Obesity";
+    }
+    console.log(bmiValue);
+
+}
+
+btnEl.addEventListener("click", calculateBMI)
